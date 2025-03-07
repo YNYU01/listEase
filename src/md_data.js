1d1b4f94fc8e610d3f04d866222b7f738ec3cfd0
lvynyu <lvynyu2@gmail.com>
2025-03-07 18:05:06 +0800

    更新

---
+++ src/md_data.js
@@ -0,0 +1,10 @@
+const models = [
+    {name:"江湖快意",img:"url()"},
+    {name:"清新古风",img:"url()"},
+    {name:"暗黑神秘",img:"url()"},
+    {name:"科技硬核",img:"url()"},
+    {name:"搞怪有趣",img:"url()"},
+    {name:"唯美梦幻",img:"url()"},
+    {name:"雍容华贵",img:"url()"},
+    {name:"节日喜庆",img:"url()"},
+]

25d822821097260cee049e18e30fcfd5efdfb37a
lvynyu <lvynyu2@gmail.com>
2025-03-12 15:58:09 +0800

    更新

--- src/md_data.js
+++ src/md_data.js
@@ -1,5 +1,5 @@
 const models = [
-    {name:"布局示意",img:"https://cdn.jsdelivr.net/gh/YNYU01/listEase@refs/heads/main/img/Icon-ListEase_200-6.png"},
+    {name:"布局示意",img:""},
     {name:"江湖快意",img:""},
     {name:"清新古风",img:""},
     {name:"暗黑神秘",img:""},
